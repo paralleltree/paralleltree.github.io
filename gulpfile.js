754ddb77d0e6a19b9a3de5874d59a1550a333a06
Paralleltree <paralleltree@outlook.com>
2018-11-23 17:15:29 +0900

    gulpfileのパス周り書き直し

--- gulpfile.js
+++ gulpfile.js
@@ -3,32 +3,35 @@ const gulp = require('gulp');
 const slim = require('gulp-slim');
 const sass = require('gulp-sass');
 
-const SLIM_PATH = './src/slim/**/*.slim';
-const SASS_PATH = './src/sass/**/*.sass';
-const IMG_PATH = './src/img/**/*';
+const SLIM_PATH = './src/slim';
+const SLIM_FILES = path.join(SLIM_PATH, '**/*.slim');
+const SASS_PATH = './src/sass';
+const SASS_FILES = path.join(SASS_PATH, '**/*.sass');
+const IMG_PATH = './src/img';
+const IMG_FILES = path.join(IMG_PATH, '**/*');
 const DEST_PATH = './dist';
 
 gulp.task('slim', () => {
-  gulp.src(SLIM_PATH)
+  gulp.src(SLIM_FILES)
     .pipe(slim())
     .pipe(gulp.dest(DEST_PATH));
 });
 
 gulp.task('sass', () => {
-  gulp.src(SASS_PATH)
+  gulp.src(SASS_FILES)
     .pipe(sass({ outputStyle: 'compressed' }))
     .pipe(gulp.dest(DEST_PATH));
 });
 
 gulp.task('image', () => {
-  gulp.src(IMG_PATH)
+  gulp.src(IMG_FILES)
     .pipe(gulp.dest(path.join(DEST_PATH, 'img')));
 });
 
 gulp.task('watch', ['build'], (done) => {
-  gulp.watch([SLIM_PATH], ['slim']);
-  gulp.watch([SASS_PATH], ['sass']);
-  gulp.watch([IMG_PATH], ['image']);
+  gulp.watch([SLIM_FILES], ['slim']);
+  gulp.watch([SASS_FILES], ['sass']);
+  gulp.watch([IMG_FILES], ['image']);
   done();
 });
 
